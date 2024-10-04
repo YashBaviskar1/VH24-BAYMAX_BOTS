7207bd29a37e4810f3b9bda4964d4874a7436053
Akshataa1804 <akshata18.2004@gmail.com>
2024-10-04 10:29:48 +0530

    initial basic templetle for login signup interface

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# Mock database (use a real database in production)
+users = {}
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users:
+            return "User already exists!"
+        users[username] = password
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            return redirect(url_for('home'))
+        return "Invalid credentials!"
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
